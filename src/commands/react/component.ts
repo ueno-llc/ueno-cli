67ff257a691e7f41013ee02b0cd53b8841710b0a
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-08-08 19:16:17 -0400

    Added sample API to component builder

--- src/commands/react/component.ts
+++ src/commands/react/component.ts
@@ -1,6 +1,24 @@
+import { Arguments, Argv } from 'yargs';
+
 export const command = 'component [name]';
 export const desc = 'initialize react component';
-export const builder = {};
-export const handler = () => {
-  console.log('TODO');
+export const builder = (yargs: Argv) =>
+  yargs
+    .option('style', {
+      describe: 'Create and import styles from file',
+      choices: ['scss', 'stylus', 'less', 'css'],
+    })
+    .option('ext', {
+      describe: 'Component file extension (tsx for TypeScript)',
+      default: 'js',
+      choices: ['js', 'jsx', 'tsx'],
+    })
+    .option('pure', {
+      describe: 'Use PureComponent',
+      default: false,
+      type: 'boolean',
+    });
+
+export const handler = (args: Arguments) => {
+  console.log('TODO', args);
 };
